67f4c12e6038459d3b2515a65920a428be62d2c6
Fred Myers <fred@fred-myers.com>
2014-04-29 11:21:45 -0400

    Initial Commit

---
+++ BrickBreaker/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  BrickBreaker
+//
+//  Created by Fredrick Myers on 4/29/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
